0f0efad71dd61a2e8dd448237a93a8f20dd00b6d
rama <rama.alshaban@gmail.com>
2022-09-18 01:44:58 +0300

    editing blog file

--- blog/apps.py
+++ blog/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+# comment just ti test something not related to the project@ 
